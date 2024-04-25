360a6defe7908728c46346582866cc8fcd3c867b
Livia2710 <130663916+Livia2710@users.noreply.github.com>
2024-04-25 18:36:59 -0300

    testes

--- crescendia/src/components/layout/navbar.js
+++ crescendia/src/components/layout/navbar.js
@@ -1,36 +1,70 @@
-import React, { Component } from 'react';
-import './navbar.css';
+// import React, { Component } from 'react';
+// import './navbar.css';
 
-class navbar extends Component {
-  state={clicked: false};
-  handelClick = () =>{
-    this.setState({clicked:!this.state.clicked})
-  }
-  render() {
+// class Navbar extends Component {
+//   state={clicked: false};
+//   handelClick = () =>{
+//     this.setState({clicked:!this.state.clicked})
+//   }
+//   render() {
 
 
-  return (
-    <>
-    <nav>
-      <a href="" alt="Logo"></a>
+//   return (
+//     <>
+//     <nav>
+//       <a href="" alt="Logo"></a>
 
-      <ul id='navbar'>
-        <li><a href="">Seu perfil</a></li>
-        <li><a href="">Biblioteca</a></li>
-        <li><a href="">Assinaturas</a></li>
-      </ul>
+//       <ul id='navbar'>
+//         <li><a href="">Seu perfil</a></li>
+//         <li><a href="">Biblioteca</a></li>
+//         <li><a href="">Assinaturas</a></li>
+//       </ul>
 
-      <div id='mobile' onClick={this.handelClick}>
-        <p id='bar' className={this.state.clicked ? "O": "X"}></p>
+//       <div id='mobile' onClick={this.handelClick}>
+//         <p id='bar' className={this.state.clicked ? "O": "X"}></p>
     
-      </div>
-    </nav>
+//       </div>
+//     </nav>
 
   
-    </>
-  );
+//     </>
+//   );
+// }
+
+// }
+
+// export default navbar
+
+
+import React, { Component } from 'react';
+import './Navbar.css';
+
+class Navbar extends Component {
+    state = { clicked: false };
+
+    handleClick = () => {
+        this.setState(prevState => ({ clicked: !prevState.clicked }));
+    }
+
+    render() {
+        return (
+            <nav>
+                <a href="#" alt="Logo"> 
+                    <img src="path_to_logo.png" alt="Logo" /> 
+                </a>
+
+                <ul id="navbar">
+                    <li><a href="#">Seu perfil</a></li>
+                    <li><a href="#">Biblioteca</a></li>
+                    <li><a href="#">Assinaturas</a></li>
+                </ul>
+
+                <button id="mobile" onClick={this.handleClick} role="button"> 
+                    <p id="bar" className={this.state.clicked ? "O" : "X"}></p>
+                </button>
+            </nav>
+        );
+    }
 }
 
-}
-
-export default navbar
+export default Navbar;
