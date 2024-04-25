f19fdab5846be8d8050a6da07fb2511211941da6
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-04-25 15:56:01 -0300

    update

--- crescendia/src/App.js
+++ crescendia/src/App.js
@@ -3,13 +3,7 @@ import navbar from './components/layout/navbar';
 import './App.css';
 
 function App() {
-  return (
-    
-    <BrowserRouter>
-      <navbar title = '' />
-    </BrowserRouter>
-    
-  );
+
 }
 
 export default App;
