e7a6799e6cd874cede43cb4a35316605314c37ae
Victor Oliveira Rodrigues <victor.orodrigues07senai@gmail.com>
2024-04-26 11:20:10 -0300

    Playlist

---
+++ crescendia/src/components/layout/playlist.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import style from './playlist.css'
+import banner from '../../assets/banner.png'
+import Navbar from './navbar'
+import Footer from './footer'
+
+
+const playlist = () => {
+    return(
+    
+
+    <div>
+        <Navbar/>
+        <div className="banner">
+            <img src={banner} alt=''></img>
+        </div>
+
+    </div>
+        
+    );
+}
