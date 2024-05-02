6a118ac1749e276233296aff3721a928c132f858
Sophia RK <130664417+s0pa-fria@users.noreply.github.com>
2024-05-02 09:15:27 -0300

    asdfgh

--- SpotifyDoCrescendia/spotify-react-clone-2024/src/views/Home.js
+++ SpotifyDoCrescendia/spotify-react-clone-2024/src/views/Home.js
@@ -6,7 +6,7 @@ import Curtidas from '../public/Icon-curtidos.png'
 import Decadente from '../public/card-playlist.png'
 
 import Foto from '../public/card-intg1.png'
-// import Foto2 from '../public/card-intg2.png'
+import Foto2 from '../public/card-intg2.png'
 import Foto3 from '../public/card-intg3.png'
 import Foto4 from '../public/card-intg4.png'
 import Foto5 from '../public/card-intg5.png'
@@ -16,6 +16,7 @@ import Mix from '../public/card-mix1.png'
 import Mix1 from '../public/card-mix2.png'
 import Mix2 from '../public/card-mix3.png'
 import Mix3 from '../public/card-mix4.png'
+import Mix4 from '../public/card-mix5.png'
 
 
 import { NavLink } from 'react-router-dom';
@@ -31,77 +32,45 @@ export default function Home() {
       id: 1,
       title: 'Mix 1',
       artist: 'Eren Yalçın',
-      image: 'https://i.scdn.co/image/ab67616d00001e02e9c9fc7a8155861f8db6b28f',
-      src: 'https://freesound.org/data/previews/617/617443_1648170-lq.mp3'
+      image: Mix,
+      src:'../public/card-mix1.png'
     },
 
     {
       id: 2,
-      title: 'Uzunlar',
+      title: 'Mix 2',
       artist: 'Evdeki Saat',
-      image: 'https://i.scdn.co/image/ab67616d00001e02917b71d0a9aa3396f0031c40',
-      src:'https://freesound.org/data/previews/617/617382_7037-lq.mp3'
+      image: Mix1,
+      src: '../public/card-mix2.png'
     },
 
     {
       id: 3,
-      title: 'Kan',
+      title: 'Mix 3',
       artist: 'UZI',
-      image: 'https://i.scdn.co/image/ab67616d00001e0267c738a703dc979f5c3c52ef',
-      src:'https://freesound.org/data/previews/617/617306_5674468-lq.mp3'
+      image: Mix2,
+      src: '../public/card-mix3.png'
     },
 
    {
       id: 4,
-      title: 'Seni Kendime Sakladım',
+      title: 'Mix 4',
       artist: 'Duman',
-      image: 'https://i.scdn.co/image/ab67616d00001e02d5a587c7de8efc3ba32bede7',
-      src:'https://freesound.org/data/previews/617/617305_11861866-lq.mp3'
+      image: Mix3,
+      src: '../public/card-mix4.png'
     },
 
     {
       id: 5,
-      title: 'Seni Dert Etmeler',
+      title: 'Mix 5',
       artist: 'Madrigal',
-      image: 'https://i.scdn.co/image/ab67616d00001e029aa96c06d5c346ccf6ffaf8d',
-      src:'https://freesound.org/data/previews/617/617303_1015240-lq.mp3'
+      image: Mix4,
+      src:'../public/card-mix5.png'
     },
 
     
   ]
 
-  const singer =[
-    {
-      id: 1,
-      position: 'Doja Cat',
-      name: 'Doja Cat',
-      image: 'https://i.scdn.co/image/ab6761610000517493bfdfa9be5d750ef6466886'
-    },
-    {
-      id: 2,
-      position: 'Bruno Mars',
-      name: 'Bruno Mars',
-      image: 'https://i.scdn.co/image/ab67616d00001e02318b9f5934c2f40343ca97c3'
-    },
-    {
-      id: 3,
-      position: 'Queen',
-      name: 'Queen',
-      image: 'https://i.scdn.co/image/ab676161000051748911c4deb367bd155bf475d0'
-    },
-    {
-      id: 4,
-      position: 'Sanatçı',
-      name: 'Nil Karaibrahimgil',
-      image: 'https://i.scdn.co/image/ab67616100005174c65a994a243b0a881a25017e'
-    },
-    {
-      id: 5,
-      position: 'Sanatçı',
-      name: 'Sezen Aksu',
-      image: 'https://i.scdn.co/image/ab67616100005174b624695e7b95277a7e29adb6'
-    }
-  ]
 
   const mixer =[
     {
@@ -136,7 +105,38 @@ export default function Home() {
     }
 
   ]
-
+  const singer =[
+    {
+      id: 1,
+      position: 'Doja Cat',
+      name: 'Doja Cat',
+      image: 'https://i.scdn.co/image/ab6761610000517493bfdfa9be5d750ef6466886'
+    },
+    {
+      id: 2,
+      position: 'Bruno Mars',
+      name: 'Bruno Mars',
+      image: 'https://i.scdn.co/image/ab67616d00001e02318b9f5934c2f40343ca97c3'
+    },
+    {
+      id: 3,
+      position: 'Queen',
+      name: 'Queen',
+      image: 'https://i.scdn.co/image/ab676161000051748911c4deb367bd155bf475d0'
+    },
+    {
+      id: 4,
+      position: 'Sanatçı',
+      name: 'Nil Karaibrahimgil',
+      image: 'https://i.scdn.co/image/ab67616100005174c65a994a243b0a881a25017e'
+    },
+    {
+      id: 5,
+      position: 'Sanatçı',
+      name: 'Sezen Aksu',
+      image: 'https://i.scdn.co/image/ab67616100005174b624695e7b95277a7e29adb6'
+    }
+  ]
 
 
   return (
@@ -172,7 +172,7 @@ export default function Home() {
         
         
         <div className='flex items-center gap-x-4 bg-dortbox group relative hover:bg-dortboxact rounded'>
-          <img className='w-[5rem] h-[5rem]' //src={Foto2} 
+          <img className='w-[5rem] h-[5rem]' src={Foto2} 
           />
           <h4 className='text-[16px] font-bold p-4'>Lívia Figueiredo</h4>
           <button className='w-12 h-12 bg-primary absolute right-2 transition-opacity rounded-full flex items-center shadow-2xl justify-center opacity-0 group-hover:opacity-100 group-active:opacity-100'>
