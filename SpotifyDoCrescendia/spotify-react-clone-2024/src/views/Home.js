2ea64962d65db82012b301399c80aebe6fc3143f
Sophia RK <130664417+s0pa-fria@users.noreply.github.com>
2024-05-02 10:04:48 -0300

    eu tenteeeei

--- SpotifyDoCrescendia/spotify-react-clone-2024/src/views/Home.js
+++ SpotifyDoCrescendia/spotify-react-clone-2024/src/views/Home.js
@@ -249,7 +249,7 @@ export default function Home() {
                     <NavLink key={item.id} to="/" className={'bg-footer rounded p-4 transition-all hover:bg-menubg group'}>
                         <div className='relative '> 
                           <img className='w-auto h-auto inset-0 object-cover mb-4 rounded-full' src={item.image} />
-                          <button className='w-12 h-12 bg-primary absolute right-2 bottom-2 transition-opacity rounded-full flex items-center shadow-2xl justify-center opacity-0 group-hover:opacity-100 group-active:opacity-100'>
+                          <button className='w-12 h-12 bg-primary absolute right-2 bottom-2 transition-opacity rounded-full flex items-center shadow-2xl justify-center opacity-0 group-hover:opacity-100 group-active:opacity-100' >
                             <Icon name="play"/>
                           </button>
                         </div>
